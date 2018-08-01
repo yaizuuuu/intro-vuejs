c754060050536f990a8a66c426618464b9aa802f
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-08-01 13:59:51 +0900

    webpackの設定に関するTODOを追加

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,12 +1,15 @@
-const { VueLoaderPlugin } = require('vue-loader');
+const {VueLoaderPlugin} = require('vue-loader')
 
 // [定数] webpack の出力オプションを指定します
 // 'production' か 'development' を指定
-const MODE = 'development';
+const MODE = 'development'
 
 // ソースマップの利用有無(productionのときはソースマップを利用しない)
-const enabledSourceMap = (MODE === 'development');
+const enabledSourceMap = (MODE === 'development')
 
+/**
+ * TODO: module.exports の中身をproductionとdeveopmentで分けれるように別ファイルにする
+ */
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
@@ -15,23 +18,32 @@ module.exports = {
   // source-map 方式でないと、CSSの元ソースが追跡できないため
   devtool: 'source-map',
 
+  /**
+   * TODO: Chapterごとにファイルが分けられるようにentryとoutputを変更する
+   */
   // メインとなるJavaScriptファイル（エントリーポイント）
   entry: `./src/index.js`,
-
   // ファイルの出力設定
   output: {
     //  出力ファイルのディレクトリ名
     path: `${__dirname}/dist`,
     // 出力ファイル名
-    filename: 'main.js'
+    filename: 'main.js',
   },
 
   module: {
     rules: [
+      /**
+       * TODO: Componentsに対応した設定を覚える
+       */
+
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: 'vue-loader',
       },
+      /**
+       * TODO: Babelの細かい設定とそれぞれの意味を覚える
+       */
       {
         test: /\.js$/,
         loader: 'babel-loader',
@@ -41,9 +53,9 @@ module.exports = {
             // env を指定することで、ES2018 を ES5 に変換。
             // {modules: false}にしないと import 文が Babel によって CommonJS に変換され、
             // webpack の Tree Shaking 機能が使えない
-            ['env', {'modules': false}]
-          ]
-        }
+            ['env', {'modules': false}],
+          ],
+        },
       },
       // Sassファイルの読み込みとコンパイル
       {
@@ -67,7 +79,7 @@ module.exports = {
               // 0 => no loaders (default);
               // 1 => postcss-loader;
               // 2 => postcss-loader, sass-loader
-              importLoaders: 2
+              importLoaders: 2,
             },
           },
           // PostCSSのための設定
@@ -79,8 +91,8 @@ module.exports = {
               plugins: [
                 // autoprefixerを有効化
                 // ベンダープレフィックスを自動付与する
-                require('autoprefixer')({grid: true})
-              ]
+                require('autoprefixer')({grid: true}),
+              ],
             },
           },
           // Sassをバンドルするための機能
@@ -89,10 +101,13 @@ module.exports = {
             options: {
               // ソースマップの利用有無
               sourceMap: enabledSourceMap,
-            }
-          }
+            },
+          },
         ],
       },
+      /**
+       * TODO: ちゃんと設定できているか調べる
+       */
       {
         // 対象となるファイルの拡張子
         test: /\.(gif|png|jpg|eot|wof|woff|ttf|svg)$/,
@@ -102,24 +117,24 @@ module.exports = {
             loader: 'url-loader',
             options: {
               limit: 100 * 1024, // 100KB以上だったら埋め込まずファイルとして分離する
-              name: './img/[name].[ext]'
-            }
-          }
-        ]
-      }
+              name: './img/[name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
   // Webpackでvueを利用するときの設定
   resolve: {
 
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
     },
-    extensions: ['*', '.js', '.vue', '.json']
+    extensions: ['*', '.js', '.vue', '.json'],
   },
   plugins: [
     // Vueを読み込めるようにするため
     new VueLoaderPlugin,
   ],
 
-};
+}
