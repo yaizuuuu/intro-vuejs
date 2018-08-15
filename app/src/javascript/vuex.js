9a2011d8047b7dcb3bbb289a7eb608fefbb8a03c
yaizuuuu <p.i.o.y.m.a.a@gmail.com>
2018-08-15 15:05:31 +0900

    fix

--- app/src/javascript/vuex.js
+++ app/src/javascript/vuex.js
@@ -1,10 +1,7 @@
 import Vue from "vue";
-import Vuex from "vuex";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
-
-Vue.use(Vuex);
 
 window.vm = new Vue({
   el: "#chapter8-44",
